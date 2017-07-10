f29d8fcda7b19c03e78bb05cd1d3a79338cd8589
Alex Bui <cuongbt91@gmail.com>
2017-07-10 14:02:12 +0900

    add device selection

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 import argparse
 import os
+import properties as p
 
 from model import Model, Config
 
@@ -38,69 +39,69 @@ print('Start training DMN on babi task', config.task_id)
 best_overall_val_loss = float('inf')
 
 # create model
+with tf.device('/%s' % p.device):
+    model = Model(config)
 
-model = Model(config)
+    for run in range(num_runs):
 
-for run in range(num_runs):
+        print('Starting run', run)
 
-    print('Starting run', run)
+        print('==> initializing variables')
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
 
-    print('==> initializing variables')
-    init = tf.global_variables_initializer()
-    saver = tf.train.Saver()
+        with tf.Session() as session:
 
-    with tf.Session() as session:
+            sum_dir = 'summaries/train/' + time.strftime("%Y-%m-%d %H %M")
+            if not os.path.exists(sum_dir):
+                os.makedirs(sum_dir)
+            train_writer = tf.summary.FileWriter(sum_dir, session.graph)
 
-        sum_dir = 'summaries/train/' + time.strftime("%Y-%m-%d %H %M")
-        if not os.path.exists(sum_dir):
-            os.makedirs(sum_dir)
-        train_writer = tf.summary.FileWriter(sum_dir, session.graph)
+            session.run(init)
 
-        session.run(init)
+            best_val_epoch = 0
+            prev_epoch_loss = float('inf')
+            best_val_loss = float('inf')
+            best_val_accuracy = 0.0
 
-        best_val_epoch = 0
-        prev_epoch_loss = float('inf')
-        best_val_loss = float('inf')
-        best_val_accuracy = 0.0
+            if args.restore:
+                print('==> restoring weights')
+                saver.restore(session, 'weights/task' +
+                            str(model.config.task_id) + '.weights')
 
-        if args.restore:
-            print('==> restoring weights')
-            saver.restore(session, 'weights/task' +
-                          str(model.config.task_id) + '.weights')
+            print('==> starting training')
+            for epoch in range(config.max_epochs):
+                print('Epoch {}'.format(epoch))
+                start = time.time()
 
-        print('==> starting training')
-        for epoch in range(config.max_epochs):
-            print('Epoch {}'.format(epoch))
-            start = time.time()
+                train_loss, train_accuracy = model.run_epoch(
+                    session, model.train, epoch, train_writer,
+                    train_op=model.train_step, train=True)
+                valid_loss, valid_accuracy = model.run_epoch(session, model.valid)
+                print('Training loss: {}'.format(train_loss))
+                print('Validation loss: {}'.format(valid_loss))
+                print('Training accuracy: {}'.format(train_accuracy))
+                print('Vaildation accuracy: {}'.format(valid_accuracy))
 
-            train_loss, train_accuracy = model.run_epoch(
-                session, model.train, epoch, train_writer,
-                train_op=model.train_step, train=True)
-            valid_loss, valid_accuracy = model.run_epoch(session, model.valid)
-            print('Training loss: {}'.format(train_loss))
-            print('Validation loss: {}'.format(valid_loss))
-            print('Training accuracy: {}'.format(train_accuracy))
-            print('Vaildation accuracy: {}'.format(valid_accuracy))
+                if valid_loss < best_val_loss:
+                    best_val_loss = valid_loss
+                    best_val_epoch = epoch
+                    if best_val_loss < best_overall_val_loss:
+                        print('Saving weights')
+                        best_overall_val_loss = best_val_loss
+                        best_val_accuracy = valid_accuracy
+                        saver.save(session, 'weights/task' +
+                                str(model.config.task_id) + '.weights')
 
-            if valid_loss < best_val_loss:
-                best_val_loss = valid_loss
-                best_val_epoch = epoch
-                if best_val_loss < best_overall_val_loss:
-                    print('Saving weights')
-                    best_overall_val_loss = best_val_loss
-                    best_val_accuracy = valid_accuracy
-                    saver.save(session, 'weights/task' +
-                               str(model.config.task_id) + '.weights')
+                # anneal
+                if train_loss > prev_epoch_loss * model.config.anneal_threshold:
+                    model.config.lr /= model.config.anneal_by
+                    print('annealed lr to %f' % model.config.lr)
 
-            # anneal
-            if train_loss > prev_epoch_loss * model.config.anneal_threshold:
-                model.config.lr /= model.config.anneal_by
-                print('annealed lr to %f' % model.config.lr)
+                prev_epoch_loss = train_loss
 
-            prev_epoch_loss = train_loss
+                if epoch - best_val_epoch > config.early_stopping:
+                    break
+                print('Total time: {}'.format(time.time() - start))
 
-            if epoch - best_val_epoch > config.early_stopping:
-                break
-            print('Total time: {}'.format(time.time() - start))
-
-        print('Best validation accuracy:', best_val_accuracy)
+            print('Best validation accuracy:', best_val_accuracy)
