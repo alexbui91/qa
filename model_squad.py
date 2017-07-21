dfe85a91b504883ebaebff765a7fe992057a762d
Alex Bui <cuongbt91@gmail.com>
2017-07-21 15:56:56 +0900

    fix validation

--- model_squad.py
+++ model_squad.py
@@ -24,6 +24,7 @@ class ModelSquad(Model):
 
     def __init__(self, config):
         self.config = config
+        self.is_training = True
 
     def set_data(self, train, valid, word_embedding, max_q_len, \
                 max_input_len, max_answer_len, vocab_len):
@@ -224,7 +225,11 @@ class ModelSquad(Model):
         return fact_vecs
 
     def get_answer_representation(self, placeholder, embeddings):
-        return tf.nn.embedding_lookup(embeddings, placeholder)
+        if self.is_training:
+            an_v = tf.nn.embedding_lookup(embeddings, placeholder)
+        else:
+            an_v = tf.zeros_like(placeholder)
+        return an_v
 
     def add_loss_op(self, output):
         output_s, output_e = output
@@ -283,6 +288,7 @@ class ModelSquad(Model):
         return pred_s, pred_e
 
     def run_epoch(self, session, data, num_epoch=0, train_writer=None, train_op=None, verbose=2, train=False):
+        self.is_training = train
         config = self.config
         dp = config.dropout
         if train_op is None:
