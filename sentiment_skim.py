a6decbae1d4bae0da8c3793a70637b20290d0bb7
Alex Bui <cuongbt91@gmail.com>
2017-12-28 11:20:18 +0900

    reverse to single gpu

--- sentiment_skim.py
+++ sentiment_skim.py
@@ -15,57 +15,50 @@ import properties as p
 class ModelSentiment():
 
 
-    def __init__(self, word_embedding=None, max_input_len=None, using_compression=False, book=None, words=None, num_gpus=0, use_multiple_gpu=False):
+    def __init__(self, word_embedding=None, max_input_len=None, using_compression=False, book=None, words=None):
         self.word_embedding = word_embedding
         self.max_input_len = max_input_len
         self.using_compression = using_compression
         self.book = book
         self.words = words
-        self.num_gpus = num_gpus
-        self.use_multiple_gpu = use_multiple_gpu
-        self.iteration = tf.placeholder(tf.int32)
-        self.dropout_placeholder = tf.placeholder(tf.float32)
 
     def set_data(self, train, valid):
         self.train = train
         self.valid = valid
-           
+        
     def set_embedding(self):
         self.word_embedding = word_embedding
     
-    def init_ops(self, input_vector=None, input_lens=None, labels=None):
-        # init memory
-        if not self.use_multiple_gpu:
-            _, _, _ = self.add_placeholders()
-        # init model
-        output = self.inference()
-        # init prediction step
-        pred = self.get_predictions(output)
-        # init cost function
-        calculate_loss = self.add_loss_op(output, labels)
-        # init gradient
-        self.train_step = self.add_training_op(calculate_loss)
-        if not elf.use_multiple_gpu:
-            self.output, self.pred, self.calculate_loss,  = output, pred, calculate_loss
-            self.merged = tf.summary.merge_all()    
-        else:
-            pred = tf.reduce_sum(tf.cast(tf.equal(pred, labels), tf.int32))
-        return pred, calculate_loss
-        
+    def init_ops(self):
+        with tf.device('/%s' % p.device):
+            # init memory
+            self.add_placeholders()
+            # init model
+            self.output = self.inference()
+            # init prediction step
+            self.pred = self.get_predictions(self.output)
+            # init cost function
+            self.calculate_loss = self.add_loss_op(self.output)
+            # init gradient
+            self.train_step = self.add_training_op(self.calculate_loss)
+
+        self.merged = tf.summary.merge_all()
+
     def add_placeholders(self):
         """add data placeholder to graph """
-        input_placeholder = tf.placeholder(tf.int32, shape=(
+        
+        self.input_placeholder = tf.placeholder(tf.int32, shape=(
             p.batch_size, self.max_input_len))  
 
-        input_len_placeholder = tf.placeholder(
+        self.input_len_placeholder = tf.placeholder(
             tf.int32, shape=(p.batch_size,))
-        pred_placeholder = tf.placeholder(
+        self.pred_placeholder = tf.placeholder(
             tf.int32, shape=(p.batch_size,))
-        if not self.use_multiple_gpu:
-            self.input_placeholder, self.input_len_placeholder, self.pred_placeholder = input_placeholder, input_len_placeholder, pred_placeholder
-        return input_placeholder, input_len_placeholder, pred_placeholder
+        # place holder for start vs end position
+        self.dropout_placeholder = tf.placeholder(tf.float32)
+        self.iteration = tf.placeholder(tf.int32)
 
-    def inference(self, input_vector=None, input_lens=None):
+    def inference(self):
         """Performs inference on the DMN model"""
 
         # set up embedding
@@ -76,18 +69,17 @@ class ModelSentiment():
        
         with tf.variable_scope("input", initializer=tf.contrib.layers.xavier_initializer()):
             print('==> get input representation')
-            if not self.use_multiple_gpu:
-                word_reps = self.get_input_representation(embeddings)
-            else:
-                word_reps = self.get_input_representation(embeddings, input_vector, input_lens)
+            word_reps = self.get_input_representation(embeddings)
             word_reps = tf.reduce_mean(word_reps, axis=1)
             # print(word_reps)
 
         with tf.variable_scope("hidden", initializer=tf.contrib.layers.xavier_initializer()):
+            
             # output = tf.layers.dense(word_reps,
             #                         p.embed_size,
             #                         activation=tf.nn.tanh,
             #                         name="h1")
+
             output = tf.layers.dense(word_reps,
                                     p.hidden_size,
                                     activation=tf.nn.tanh,
@@ -105,17 +97,14 @@ class ModelSentiment():
         w = tf.Variable(self.words, name="words", trainable=False)
         return b, w
 
-    def get_input_representation(self, embeddings, input_vector=None, input_lens=None):
+    def get_input_representation(self, embeddings):
         """Get fact (sentence) vectors via embedding, positional encoding and bi-directional GRU"""
-        if not self.use_multiple_gpu:
-            input_lens = self.input_len_placeholder
-            input_vector = self.input_placeholder
         inputs = None
         if self.using_compression:
             b_embedding, w_embedding = self.build_book()
             # from code words => build one hot
             # B x L x M: batchsize x length_sentence
-            d = tf.nn.embedding_lookup(w_embedding, input_vector)
+            d = tf.nn.embedding_lookup(w_embedding, self.input_placeholder)
             # => B x L x M x K
             d_ = tf.reshape(d, [-1])
             d_ = tf.one_hot(d_, depth=p.code_size, axis=-1)
@@ -125,29 +114,28 @@ class ModelSentiment():
             inputs = tf.reshape(tf.reshape(inputs, [-1]), [p.batch_size, self.max_input_len, p.embed_size])
         else:
             # get word vectors from embedding
-            inputs = tf.nn.embedding_lookup(embeddings, input_vector)
+            inputs = tf.nn.embedding_lookup(embeddings, self.input_placeholder)
         # chunking_len = int(self.max_input_len / p.fixation)
         # inputs = tf.reshape(tf.reshape(inputs, [-1]), [p.batch_size, chunking_len, p.fixation * p.embed_size])
         # use encoding to get sentence representation plus position encoding
         # (like fb represent)
         gru_cell = BasicLSTMCell(p.embed_size)
         # outputs with [batch_size, max_time, cell_bw.output_size]
-        
         outputs, _ = tf.nn.dynamic_rnn(
             gru_cell,
             inputs,
             dtype=np.float32,
-            sequence_length=input_lens,
+            sequence_length=self.input_len_placeholder,
         )
 
         return outputs
 
 
-    def add_loss_op(self, output, labels):
+    def add_loss_op(self, output):
         """Calculate loss"""
 
         loss = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=output, labels=labels))
+            logits=output, labels=self.pred_placeholder))
         
         # add l2 regularization for all variables except biases
         for v in tf.trainable_variables():
@@ -173,95 +161,49 @@ class ModelSentiment():
         pred = tf.argmax(pred, 1)
         return pred
 
-    def shuffle_batch(self, data):
-        ct, ct_l, pr = data
-        total_steps = len(ct) // p.batch_size
-        context, length, pred = tf.train.shuffle_batch(
-                                [ct, ct_l, pr], 
-                                p.batch_size, 
-                                enqueue_many=True, 
-                                num_threads=16,
-                                capacity=total_steps)
-        return tf.contrib.slim.prefetch_queue.prefetch_queue([context, length, pred], capacity =  2 * self.num_gpus)
-
-    # prepare data for multiple gpu
-    def prepare_gpu_data(self):
-        self.batch_queue = self.shuffle_batch(self.train)
-        self.batch_queue_valid = self.shuffle_batch(self.valid)
-        self.input_placeholder, self.inputs_len, self.pred_placeholder = [], [], []
-        for i in xrange(self.num_gpus):
-            ip, il, pl = self.add_placeholders();
-            self.input_placeholder.append(ip)
-            self.input_len_placeholder.append(il),
-            self.pred_placeholder.append(pl)
-    
-    def debug(self, step, total_steps, total_loss):
-        sys.stdout.write('\r{} / {} : loss = {}'.format(
-            step, total_steps, total_loss))
-        sys.stdout.flush()
-
-    # prepare machine to run multiple gpu
-    def setup_multiple_gpu(self):
-        with tf.variable(tf.get_variable_scope()):
-            for i in xrange(self.num_gpus):
-                with tf.device('/gpu:%i' % i):
-                    ip = self.input_placeholder[i]
-                    il = self.input_len_placeholder[i] 
-                    pl = self.pred_placeholder[i]
-                    loss, pred = self.init_ops(ip, il, pl)
-                    feed = {
-                        input_placeholder : context_batch,
-                        input_len_placeholder: context_lens,
-                        self.dropout_placeholder: dp,
-                        pred_placeholder: pred_labels
-                    }
-                    tf.get_variable_scope().reuse_variables()
-        summary = tf.summary.merge_all() 
-
-    def run_epoch(self, session, data, num_epoch=0, train_writer=None, train_op=None):
+    def run_epoch(self, session, data, num_epoch=0, train_writer=None, train_op=None, verbose=2, train=False):
         dp = p.dropout
         if train_op is None:
             train_op = tf.no_op()
             dp = 1
         total_steps = len(data[0]) // p.batch_size
-        total_loss = 0
+        total_loss = []
         accuracy = 0
 
-        if not self.use_multiple_gpu:
-            ct, ct_l, pr = data
-            r = np.random.permutation(len(ct))
-            ct, ct_l, pr = np.asarray(ct, dtype=np.float32), np.asarray(ct_l, dtype=np.float32), np.asarray(pr, dtype=np.float32)
-            ct, ct_l, pr = ct[r], ct_l[r], pr[r]
-            for step in range(total_steps):
-                index = range(step * p.batch_size,
-                            (step + 1) * p.batch_size)
-                feed = {self.input_pen_placeholder: ct_l[index],
-                        self.pred_placeholder: pr[index],
-                        self.dropout_placeholder: dp,
-                        self.iteration: num_epoch}
-                
-                pred_labels = pr[step * p.batch_size:(step + 1) * p.batch_size]
-                
-                loss, pred, summary, _ = session.run(
-                    [self.calculate_loss, self.pred, self.merged, train_op], feed_dict=feed)
-                total_loss += loss
-                if train_writer is not None:
-                    train_writer.add_summary(
-                        summary, num_epoch * total_steps + step)
-                
-                accuracy += (np.sum(pred == pred_labels)) / p.batch_size
-                debug(step, total_steps, total_loss)
-        else:
-            if train_op:
-                context_batch, context_lens, pred_labels = self.batch_queue.dequeue()
-            else:
-                context_batch, context_lens, pred_labels = self.batch_queue_valid.dequeue()
-            loss, acc, _ = session.run([loss, pred, train_op], feed)
-            total_loss += loss
-            accuracy += acc
+        # shuffle data
+        r = np.random.permutation(len(data[0]))
+        ct, ct_l, pr = data
+        ct, ct_l, pr = np.asarray(ct, dtype=np.float32), np.asarray(ct_l, dtype=np.float32), np.asarray(pr, dtype=np.float32)
+        ct, ct_l, pr = ct[r], ct_l[r], pr[r]
+        for step in range(total_steps):
+            index = range(step * p.batch_size,
+                          (step + 1) * p.batch_size)
+            feed = {self.input_placeholder: ct[index],
+                    self.input_len_placeholder: ct_l[index],
+                    self.pred_placeholder: pr[index],
+                    self.dropout_placeholder: dp,
+                    self.iteration: num_epoch}
+            
+            pred_labels = pr[step * p.batch_size:(step + 1) * p.batch_size]
+            
+            loss, pred, summary, _ = session.run(
+                [self.calculate_loss, self.pred, self.merged, train_op], feed_dict=feed)
+            if train_writer is not None:
+                train_writer.add_summary(
+                    summary, num_epoch * total_steps + step)
+            
+            accuracy += (np.sum(pred == pred_labels)) / float(len(pred_labels))
+           
+            total_loss.append(loss)
 
-        sys.stdout.write('\r')
+            if verbose and step % verbose == 0:
+                sys.stdout.write('\r{} / {} : loss = {}'.format(
+                    step, total_steps, np.mean(total_loss)))
+                sys.stdout.flush()
+
+        if verbose:
+            sys.stdout.write('\r')
         avg_acc = 0.
         if total_steps:
             avg_acc = accuracy / float(total_steps)
-        return total_loss, avg_acc
+        return np.sum(total_loss), avg_acc
